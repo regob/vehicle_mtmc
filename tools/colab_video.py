a78514ccad0d9ab2b573f5cd7ea865c35ffd0385
regob <rego97@gmail.com>
2021-10-05 11:21:14 +0200

    html frame width added

--- tools/colab_video.py
+++ tools/colab_video.py
@@ -3,9 +3,10 @@ from IPython.display import HTML
 from base64 import b64encode
 import subprocess
 import time
+import os
 
 
-def play_video(path, duration="total", offset=0):
+def play_video(path, duration="total", offset=0, width=600):
     """ Returns HTML for playing an mp4 video for a duration
 
     Parameters
@@ -16,11 +17,16 @@ def play_video(path, duration="total", offset=0):
         Length of video chunk to be played in seconds.
     offset: int
         Beginning timestamp of playback in seconds.
+    width: int
+        Width of the HTML video frame.
 
     Returns
     -------
     HTML
     """
+
+    if not os.path.exists(path):
+        raise ValueError("Wrong path provided")
 
     tmp_file = "/tmp/{}.mp4".format(str(time.time()).split('.')[1])
     dur = [] if duration == "total" else ["-t", str(duration)]
@@ -32,7 +38,7 @@ def play_video(path, duration="total", offset=0):
         mp4 = f.read()
     data_url = "data:video/mp4;base64," + b64encode(mp4).decode()
     return HTML("""
-    <video width=600 controls>
+    <video width=%s controls>
         <source src="%s" type="video/mp4">
     </video>
-    """ % data_url)
+    """ % (str(width), data_url))

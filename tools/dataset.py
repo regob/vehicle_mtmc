be740ca54ad8993dd2bb46e74cba4b30d6df9ed1
regob <rego97@gmail.com>
2022-03-03 18:49:18 +0100

    merge with origin

--- tools/dataset.py
+++ tools/dataset.py
@@ -19,3 +19,26 @@ class TestDataset(Dataset):
         if self.transform:
             image = self.transform(image)
         return image, img_path
+
+
+class ImageDataset(Dataset):
+    def __init__(self, img_root, df, target_label, transform=None, target_transform=None):
+        self.img_root = img_root
+        self.df = df
+        self.target_label = target_label
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        row = self.df.loc[idx]
+        pth = os.path.join(self.img_root, row["path"])
+        image = Image.open(pth).convert("RGB")
+        label = row[self.target_label]
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label

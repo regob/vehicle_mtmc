46607ac8d1d7c34aa61aca4f73a062ef70f9a6c2
regob <rego97@gmail.com>
2022-03-04 11:21:56 +0100

    modified ImageDataset to work with ids

--- tools/dataset.py
+++ tools/dataset.py
@@ -22,12 +22,20 @@ class TestDataset(Dataset):
 
 
 class ImageDataset(Dataset):
-    def __init__(self, img_root, df, target_label, transform=None, target_transform=None):
+    def __init__(self, img_root, df, target_label, classes="infer", transform=None, target_transform=None):
         self.img_root = img_root
         self.df = df
         self.target_label = target_label
         self.transform = transform
         self.target_transform = target_transform
+        if classes == "infer":
+            self.classes = list(set(df[target_label].values))
+        else:
+            self.classes = classes
+            class_set = set(df[target_label].values)
+            assert class_set.issubset(
+                set(classes)), "Error: Classes in df are not a subset of provided classes."
+        self.class_idx = {cl: idx for idx, cl in enumerate(self.classes)}
 
     def __len__(self):
         return len(self.df)
@@ -36,7 +44,7 @@ class ImageDataset(Dataset):
         row = self.df.loc[idx]
         pth = os.path.join(self.img_root, row["path"])
         image = Image.open(pth).convert("RGB")
-        label = row[self.target_label]
+        label = self.class_idx[row[self.target_label]]
         if self.transform:
             image = self.transform(image)
         if self.target_transform:

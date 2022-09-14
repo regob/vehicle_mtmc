ffc4ce8ba9887983d375ecd5cd40c979b841ee02
regob <rego97@gmail.com>
2022-09-14 14:48:00 +0200

    config tools and examples added

---
+++ config/config_tools.py
@@ -0,0 +1,37 @@
+import os
+from yacs.config import CfgNode as CN
+
+def get_abspath(path: str, project_root: str):
+    if path is None:
+        return None
+    if os.path.isabs(path):
+        return path
+    return os.path.normpath(os.path.join(project_root, path))
+
+def expand_relative_paths(root_cfg: CN):
+    c = root_cfg
+    root = c.SYSTEM.ROOT_DIR
+    c.OUTPUT_DIR = get_abspath(c.OUTPUT_DIR, root)
+    c.MOT.VIDEO = get_abspath(c.MOT.VIDEO, root)
+    c.MOT.REID_MODEL_CKPT = get_abspath(c.MOT.REID_MODEL_CKPT, root)
+    c.MOT.REID_MODEL_OPTS = get_abspath(c.MOT.REID_MODEL_OPTS, root)
+    c.MOT.DETECTION_MASK = get_abspath(c.MOT.DETECTION_MASK, root)
+
+    # expand paths in attribute dicts
+    for x in c.MOT.STATIC_ATTRIBUTES:
+        for k, v in x.items():
+            x[k] = get_abspath(v, root)
+    for x in c.MOT.DYNAMIC_ATTRIBUTES:
+        for k, v in x.items():
+            x[k] = get_abspath(v, root)
+    c.MOT.ZONE_MASK_DIR = get_abspath(c.MOT.ZONE_MASK_DIR, root)
+
+    c.MTMC.CAMERA_LAYOUT = get_abspath(c.MTMC.CAMERA_LAYOUT, root)
+    for i, x in enumerate(c.MTMC.PICKLED_TRACKLETS):
+        c.MTMC.PICKLED_TRACKLETS[i] = get_abspath(x, root)
+
+    # expand paths in express config
+    for it in c.EXPRESS.CAMERAS:
+        for k, v in list(it.items()):
+            it[k] = get_abspath(v, root)
+    return c

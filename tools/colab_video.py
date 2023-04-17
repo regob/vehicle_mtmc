bb5947029e1d8e6555e6cdad169c7f30a122ac89
regob <rego97@gmail.com>
2023-04-17 16:05:19 +0200

    colab video not recoded if total duration is needed.

--- tools/colab_video.py
+++ tools/colab_video.py
@@ -28,13 +28,15 @@ def play_video(path, duration="total", offset=0, width=600):
     if not os.path.exists(path):
         raise ValueError("Wrong path provided")
 
-    tmp_file = "/tmp/{}.mp4".format(str(time.time()).split('.')[1])
-    dur = [] if duration == "total" else ["-t", str(duration)]
-    off = [] if offset == 0 else ["-ss", str(offset)]
-    params = ["ffmpeg", "-i", path] + dur + off + [tmp_file]
-    sproc = subprocess.run(params)
+    if duration != "total" or offset > 0:
+        tmp_file = "/tmp/{}.mp4".format(str(time.time()).split('.')[1])
+        dur = [] if duration == "total" else ["-t", str(duration)]
+        off = [] if offset == 0 else ["-ss", str(offset)]
+        params = ["ffmpeg", "-i", path] + dur + off + [tmp_file]
+        sproc = subprocess.run(params)
+        path = tmp_file
 
-    with io.open(tmp_file, 'r+b') as f:
+    with io.open(path, 'r+b') as f:
         mp4 = f.read()
     data_url = "data:video/mp4;base64," + b64encode(mp4).decode()
     return HTML("""
